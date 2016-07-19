30e0a5b10d5ee47c73370721e6a7c07cd34e6a35
UtkarshBhimte <utkarshbhimte95@gmail.com>
2016-07-19 17:23:01 +0530

    new structure desing

--- js/script.js
+++ js/script.js
@@ -1,12 +1,67 @@
 $(document).ready(function() {
+    $('#canvas').load("content.html");
 
     var width = $(window).width(),
         height = $(window).height();
-    console.log((width <= 800));
-    
-    $('select').on('focus', function() {
-        document.body.scrollTop = $(this).offset().top + (height / 2);
+    current = "home";
+
+    console.log("mobile - " + (width <= 800));
+
+    $('.btnToForm').on('click', function() {
+        if (current !== "form") {
+            current = "form";
+            $('#canvas').addClass("animated fadeOutLeft");
+            setTimeout(function() {
+              document.body.scrollTop = $('body').offset().top;
+                $('#canvas').load("form.html").removeClass("animated fadeOutLeft").addClass("animated fadeInRight");
+            }, 1000);
+        }
     });
+
+    $('#btnToHome').on('click', function() {
+        if (current !== "home") {
+            current = "home";
+            $('#canvas').addClass("animated fadeOutLeft");
+            setTimeout(function() {
+              document.body.scrollTop = $('body').offset().top;
+                $('#canvas').load("content.html").removeClass("animated fadeOutLeft").addClass("animated fadeInRight");
+            }, 1000);
+        }
+    });
+
+    $('#btnToTracks').on('click', function() {
+        if (current !== "tracks") {
+            current = "tracks";
+            $('#canvas').addClass("animated fadeOutLeft");
+
+            setTimeout(function() {
+              document.body.scrollTop = $('body').offset().top;
+                $('#canvas').load("tracks.html").removeClass("animated fadeOutLeft").addClass("animated fadeInRight");
+            }, 1000);
+        }
+    });
+
+    $('#btnToTeam').on('click', function() {
+        if (current !== "team") {
+            current = "team";
+            $('#canvas').addClass("animated fadeOutLeft");
+            setTimeout(function() {
+              document.body.scrollTop = $('body').offset().top;
+                $('#canvas').load("team.html").removeClass("animated fadeOutLeft").addClass("animated fadeInRight");
+
+                setTimeout(function() {
+                    $('.mentorsImage').removeClass("invisible").addClass("animated bounceIn");
+                    $('.mentorsName').removeClass("invisible").addClass("animated flipInX");
+                    $('.mTitle').removeClass("invisible").addClass("animated flipInX");
+                    $('#team .fa').removeClass("invisible").addClass("animated flipInX");
+
+
+                }, 300);
+            }, 1000);
+        }
+    });
+
+
 
 
     function smoothScrolling() {
@@ -24,7 +79,6 @@ $(document).ready(function() {
         });
     }
 
-    $('.scrollspy').scrollSpy();
 
     $.scrollify({
         section: "section",
@@ -41,7 +95,6 @@ $(document).ready(function() {
         afterRender: function() {}
     });
 
-    $('select').material_select();
 
     $(window).scroll(function() {
         var wScroll = $(this).scrollTop();
@@ -50,30 +103,26 @@ $(document).ready(function() {
             $('#second p').removeClass("invisible").addClass("animated bounceIn");
         }
 
-        if (wScroll > $('#three').offset().top - 50) {
+        if (wScroll > $('#three').offset().top - height/3) {
             $('#three .four').removeClass("invisible").addClass("animated fadeInLeft");
             $('#three .eight').removeClass("invisible").addClass("animated fadeinTop");
             $('#three .button').removeClass("invisible").addClass("animated bounceIn")
 
         }
 
-        if (wScroll > $('#four').offset().top - 50) {
+        if (wScroll > $('#four').offset().top - height/3) {
             $('#four .four').removeClass("invisible").addClass("animated fadeInLeft");
             $('#four .eight').removeClass("invisible").addClass("animated fadeinTop");
             $('#four .button').removeClass("invisible").addClass("animated bounceIn")
         }
 
-        if (wScroll > $('#five').offset().top - 50) {
+        if (wScroll > $('#five').offset().top - height/3) {
             $('#five .four').removeClass("invisible").addClass("animated fadeInLeft");
             $('#five .eight').removeClass("invisible").addClass("animated fadeinTop");
             $('#five .button').removeClass("invisible").addClass("animated bounceIn")
         }
 
-        if (wScroll > $('#team').offset().top - 50) {
-            $('.mentorsImage').removeClass("invisible").addClass("animated bounceIn");
-            $('.mentorsName').removeClass("invisible").addClass("animated flipInX");
-            $('.mTitle').removeClass("invisible").addClass("animated flipInX");
-            $('#team .fa').removeClass("invisible").addClass("animated flipInX");
+        if (wScroll > $('#team').offset().top) {
 
 
         }
@@ -102,6 +151,8 @@ $(document).ready(function() {
                             smoothScrolling();
                         }
                     } */
+
+    //this moves the cursor to next input field by pressing enter
     $('body').on('keydown', 'input, select, textarea', function(e) {
         var self = $(this),
             form = self.parents('form:eq(0)'),
@@ -119,9 +170,9 @@ $(document).ready(function() {
     });
     // This will fire when document is ready:
 });
-
+/*
 function submitMessage(message) {
     console.log("reached!");
     var $toastContent = $('<span>' + message + '</span>');
     Materialize.toast($toastContent, 2000);
-}
+}*/
